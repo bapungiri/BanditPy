eed741db8e0bbbdcdb2d2d80a41535329eda0f62
bapungiri <bapungiri.codespace@outlook.com>
2025-04-23 16:33:25 +0530

    performance function moved to core object

--- banditpy/analyses/bandit_task.py
+++ banditpy/analyses/bandit_task.py
@@ -12,68 +12,7 @@ from sklearn.linear_model import LogisticRegression
 from pathlib import Path
 
 
-def get_performance_2ab(
-    mab: core.MultiArmedBandit,
-    min_trials_per_sess=None,
-    roll_window=80,
-    delta_prob=None,
-    smooth=2,
-):
-    """Get performance on two armed bandit task
 
-    Parameters
-    ----------
-    df : csv file containing all data
-        _description_
-    min_trials_per_sess : _type_, optional
-        sessions with more than this number of trials will excluded.
-    roll_window : int, optional
-        no.of sessions over which performance is calculated, by default 80
-    roll_step : int, optional
-        _description_, by default 40
-    delta_prob : _type_, optional
-        _description_, by default None
-
-    Returns
-    -------
-    _type_
-        _description_
-    """
-
-    assert mab.n_ports == 2, "This task has more than 2 ports"
-    if delta_prob is not None:
-        prob_diff = np.abs(df["rewprobfull1"] - df["rewprobfull2"])
-        df = df[prob_diff >= delta_prob]
-
-    session_id = df["session#"].to_numpy()
-    unq_session_id, n_trials = np.unique(session_id, return_counts=True)
-    is_choice_high = df["is_choice_high"].to_numpy()
-
-    if min_trials_per_sess is not None:
-        bad_sessions = unq_session_id[n_trials < min_trials_per_sess]
-        n_trials = n_trials[n_trials >= min_trials_per_sess]
-        bad_trials = np.isin(session_id, bad_sessions)
-
-        is_choice_high = is_choice_high[~bad_trials]
-
-    # converting into n_sessions x n_trials dataframe
-    is_choice_high_per_session = pd.DataFrame(
-        np.split(is_choice_high.astype(int), np.cumsum(n_trials)[:-1])
-    )
-    prob_correct_per_trial = is_choice_high_per_session.mean(axis=0)
-    trial_x = np.arange(len(prob_correct_per_trial)) + 1
-
-    perf = np.array([np.mean(arr) for arr in is_choice_high_per_session])
-
-    sess_div_perf = is_choice_high_per_session.rolling(
-        window=roll_window, closed="right", min_periods=2
-    ).mean()[roll_window - 1 :: roll_window]
-
-    sess_div_perf_arr = sess_div_perf.to_numpy()
-
-    sess_div_perf_arr = gaussian_filter1d(sess_div_perf_arr, axis=1, sigma=smooth)
-
-    return sess_div_perf_arr
 
 
 class HistoryBasedLogisticModel:
