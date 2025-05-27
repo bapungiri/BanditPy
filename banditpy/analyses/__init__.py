d5dd545401dc6eb04196c0043784f6e804035d7a
bapungiri <bapungiri.codespace@outlook.com>
2025-05-27 09:35:11 +0530

    moved qlearn and regression to model folder

--- banditpy/analyses/__init__.py
+++ banditpy/analyses/__init__.py
@@ -1,3 +1,1 @@
-from .qlearn import Qlearn2Arm
-from .regression_models import Logistic2Arm
 from .switch_probability import SwitchProb2Arm
--- banditpy/models/__init__.py
+++ banditpy/models/__init__.py
@@ -1,1 +1,3 @@
+from .qlearn import Qlearn2Arm
+from .regression_models import Logistic2Arm
 from .rnn_models import BanditTrainer2Arm
