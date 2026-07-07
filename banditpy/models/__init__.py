08fd4a0e03ff82b440bbfa2a41f9be16c79840e8
bapungiri <bapungiri.codespace@outlook.com>
2026-07-07 18:08:23 +0530

    imports changed from eager to lazy

--- banditpy/models/__init__.py
+++ banditpy/models/__init__.py
@@ -1,17 +1,34 @@
 def __getattr__(name):
-    if name == 'Qlearn2Arm':
+    if name == "Qlearn2Arm":
         from .qlearn import Qlearn2Arm
+
         return Qlearn2Arm
-    if name == 'Logistic2Arm':
+    if name == "Logistic2Arm":
         from .regression_models import Logistic2Arm
+
         return Logistic2Arm
-    if name == 'BanditTrainer2Arm':
+    if name == "BanditTrainer2Arm":
         from .rnn_models import BanditTrainer2Arm
+
         return BanditTrainer2Arm
-    if name == 'Thompson2Arm':
+    if name == "Thompson2Arm":
         from .thompson_models import Thompson2Arm
+
         return Thompson2Arm
-    if name == 'DecisionModel':
+    if name == "DecisionModel":
         from .model import DecisionModel
+
         return DecisionModel
-    raise AttributeError(f'module {__name__!r} has no attribute {name!r}')
+    if name == "VanillaRNNTrainer2Arm":
+        from .rnn_fit import VanillaRNNTrainer2Arm
+
+        return VanillaRNNTrainer2Arm
+    if name == "VanillaRNNModel":
+        from .rnn_fit import VanillaRNNModel
+
+        return VanillaRNNModel
+    if name == "VanillaRNNFit2Arm":
+        from .rnn_fit import VanillaRNNFit2Arm
+
+        return VanillaRNNFit2Arm
+    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
